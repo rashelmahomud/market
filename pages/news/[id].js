8872c1817086737d0585b6f834dd9066a0850e7d
MCTC <rashelmahmudraj1998@gmail.com>
2023-04-29 16:52:01 +0600

    added soem work

--- pages/news/[id].js
+++ pages/news/[id].js
@@ -1,6 +1,9 @@
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
+import { AiOutlineArrowLeft } from 'react-icons/ai';
 import { useRouter } from "next/router";
+import Footer from "../footer";
 
 const NewsDetails = ({ post }) => {
   const router = useRouter()
@@ -16,18 +19,35 @@ const NewsDetails = ({ post }) => {
     <div>
       <Head><title> Market /News Details</title></Head>
 
-      <div className="shadow relative overflow-hidden">
-        <Image className="w-96 transition duration-300 ease-in-out hover:scale-110" src={newNews.img1} width={500} height={300} alt="product" />
-        <div className="lg:p-10 p-3">
+      <div>
+        <div className="flex gap-5 items-center font-semibold lg:px-20 p-10 bg-gray-100 my-10 lg:mt-20">
+          <Link href={'/'}>Home</Link>
+          <p><AiOutlineArrowLeft /></p>
+          <Link href={''}>market</Link>
+        </div>
+        <div>
+          <div className="text-3xl font-bold my-10 mx-20">
+            <h1 className="pb-5">Category: Speaker</h1>
+            <hr />
 
-          <h1 className="text-xl font-semibold text-blue-600">{newNews.title}</h1>
+          </div>
+
+          <div className="shadow relative overflow-hidden lg:px-20">
+            <Image className="w-96 transition duration-300 ease-in-out hover:scale-110" src={newNews.img1} width={500} height={300} alt="product" />
+            <div className="lg:p-10 p-3">
+
+              <h1 className="text-xl font-semibold text-blue-600">{newNews.title}</h1>
 
 
-          <p className="text-2xl font-bold mt-3">{newNews.dec}</p>
-          
+              <p className="text-2xl font-bold mt-3">{newNews.dec}</p>
+
+            </div>
+
+          </div>
         </div>
 
       </div>
+      <Footer />
 
     </div>
   )
