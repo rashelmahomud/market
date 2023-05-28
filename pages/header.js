7bf0c6a601a507266d1d462d20b5c7ac406251fa
MCTC <rashelmahmudraj1998@gmail.com>
2023-05-28 09:13:53 +0600

    addeds seom

--- pages/header.js
+++ pages/header.js
@@ -17,8 +17,8 @@ const Navigation = () => {
         { id: 4, name: "CONTACT", link: "/news" },
     ]
     return (
-        <div className='shadow-md w-full fixed z-10 top-0 left-0'>
-            <div className='lg:flex items-center justify-between bg-white py-4 lg:px-10 px-10'>
+        <div className=' w-full fixed z-10 top-0 left-0 bg-gradient-to-r from-orange-200 from-10% via-orange-50 via-30% to-red-50 to-90%'>
+            <div className='lg:flex items-center justify-between  py-4 lg:px-10 px-10'>
                 <div className='font-bold text-3xl cursor-pointer flex items-center font-[poppins] text-gray-800 gap-3'>
                     <span className='text-3xl text-indigo-600'><i className="fa-solid fa-headphones"></i></span>
                     Marketo.
@@ -32,7 +32,7 @@ const Navigation = () => {
 
                 </div>
 
-                <ul className={`lg:flex md:items-center md:pb-3 pb-12 absolute lg:static bg-white lg:z-auto z-[-1] left-0 w-full lg:w-auto lg:pl-0 pl-9 transition-all duration-500 ease-in ${open ? "top-18 opacity-100" : "top-[-490px] lg:opacity-100 opacity-0"}`}>
+                <ul className={`lg:flex md:items-center md:pb-3 pb-12 absolute lg:static lg:z-auto z-[-1] left-0 w-full lg:w-auto lg:pl-0 pl-9 transition-all duration-500 ease-in ${open ? "top-18 opacity-100" : "top-[-490px] lg:opacity-100 opacity-0"}`}>
 
                     {
                         links.map((link) => (
